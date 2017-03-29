2b26ddf48021a8fefbd77b8be7770b072de5754f
issac <943417187@qq.com>
2017-03-29 21:25:21 +0800

    Update index.js

--- life/js/index.js
+++ life/js/index.js
@@ -1,1 +1,342 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<62?'':e(parseInt(c/62)))+((c=c%62)>35?String.fromCharCode(c+29):c.toString(36))};if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'([efjm-oqt-vA-W]|[12]\\w)'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(A(){o 1c=[],1t=[],R,J,K,m,u,1d=0,1e=[1,2],1f=A(B,S){f.B=B;f.R=R;f.1F=S.t||M;f.18=[];L(!S.j){T(o i=0;i<4;i++){f.18[i]=N O.W.1u(B.E.x+(S.x[i]*B.19)+(S.z[i]*B.1a),B.E.y+S.y[i],B.E.z+(S.x[i]*B.1a)+(-S.z[i]*B.19))}f.18[3].next=M}},1g=A(1G,j,1h){f.j=N O.W.Image(f,1G+j.j,1,{1H:A(j){j.B.1H=13;j.B.1I(j)}});f.1i=M;f.1a=j.C;f.19=j.D;f.E=N O.W.1u(j.x,j.y,j.z);f.tx=j.x+(j.C*P.1j(m.1k)*20);f.tz=j.z-(j.D*P.1j(m.1k)*20);f.1v=[];T(o i=-1,p;p=1h[++i];){1t[i]=(p.j===13?1:2);f.1v.1L(N 1f(f,p))}},1M=A(1l){1f.1m.1w=O.W.1w;K=N O.Screen({container:"canvas"});R=K.R;K.resize();J=N O.Pointer({tap:A(){L(m.U){L(m.U===m.q.14){m.q.x=0;m.q.z=0;m.q.14=M}1n{m.q.14=m.U;m.q.x=m.U.tx;m.q.z=m.U.tz;T(o i=0,d;d=1c[i++];){o dx=m.q.x-d.E.x;o dz=m.q.z-d.E.z;o 1N=P.1j(dx*dx+dz*dz);o 1O=(1N>F)?1e[0]:1e[1];d.j.setLevel(1O)}}}}});m=N O.W.Camera({1k:P.1j(K.1z)*10,easeTranslation:0.025,easeRotation:0.06,disableRz:13},{1A:A(){f.U=M;L(J.1P){f.q.14=M;f.q.ry=-J.Xi*0.01;f.q.rx=(J.Y-K.1o*0.5)/(K.1o*0.5)}1n{L(f.q.14){f.q.ry=P.atan2(f.q.14.E.x-f.x,f.q.14.E.z-f.z)}}f.q.rx*=0.9}});m.z=-10000;m.py=0;T(o i=0,j;j=1l.1S[i++];){1c.1L(N 1g(1l.1T.1U,j,1l.1h))}setInterval(A(){1e=(1d>v)?[2,3]:[1,2];1d=0},Q);1B()},1B=A(){R.clearRect(0,0,K.1z,K.1o);m.1A();T(o k=-1,l;l=1t[++k];){u=M;T(o i=0,d;d=1c[i++];){(l===1&&d.1b())||(d.1i&&d.1v[k].1b())}}L(m.U&&!J.1P){K.1V("J")}1n{K.1V("1A")}1d++;requestAnimFrame(1B)};1f.1m.1b=A(){o c=f.1F;L(c.u||!u){o s=c.u?f.B.u:1;u="rgba("+P.1C(c.r*s)+","+P.1C(c.g*s)+","+P.1C(c.b*s)+","+(c.a||1)+")";R.fillStyle=u}L(!c.u||f.B.u<1){T(o i=0;f.18[i++].1W(););f.1w();R.t()}}1g.1m.1I=A(j){o d=[-1,1,1,-1,1,1,-1,-1];o w=j.1X.1z*0.5;o h=j.1X.1o*0.5;T(o i=0;i<4;i++){j.18[i]=N O.W.1u(f.E.x+(w*f.19*d[i]),f.E.y+(h*d[i+4]),f.E.z+(w*f.1a*d[i]))}}1g.1m.1b=A(){f.E.1W();L(f.E.Z>-(m.1k>>1)&&f.j.W(13)){f.u=0.5+P.abs(f.19*m.cosY-f.1a*m.sinY)*0.6;f.1i=13;f.j.1b();L(J.hasMoved||J.isDown){L(f.j.isPointerInside(J.X,J.Y))m.U=f}}1n f.1i=M;1Y 13}1Y{1D:A(1Z){window.addEventListener(\'1D\',A(){O.loadJS("js/imageTransform3D.js",1M,1Z)},M)}}})().1D({1S:[{j:\'G/1.H\',x:-Q,y:0,z:F,C:0,D:1},{j:\'G/2.H\',x:0,y:0,z:F,C:0,D:1},{j:\'G/3.H\',x:Q,y:0,z:F,C:0,D:1},{j:\'G/4.H\',x:F,y:0,z:Q,C:-1,D:0},{j:\'G/5.H\',x:F,y:0,z:0,C:-1,D:0},{j:\'G/6.H\',x:F,y:0,z:-Q,C:-1,D:0},{j:\'G/7.H\',x:Q,y:0,z:-F,C:0,D:-1},{j:\'G/8.H\',x:0,y:0,z:-F,C:0,D:-1},{j:\'G/9.H\',x:-Q,y:0,z:-F,C:0,D:-1},{j:\'G/10.H\',x:-F,y:0,z:-Q,C:1,D:0},{j:\'G/11.H\',x:-F,y:0,z:0,C:1,D:0},{j:\'G/12.H\',x:-F,y:0,z:Q,C:1,D:0}],1h:[{t:{r:n,g:n,b:n,u:1},x:[-22,-1p,-1p,-22],z:[-e,-e,-e,-e],y:[e,e,-e,-e]},{t:{r:n,g:n,b:n,u:1},x:[-501,2,2,-e],z:[-e,-e,-e,-e],y:[e,e,-e,-e]},{t:{r:n,g:n,b:n,u:1},x:[0,23,23,0],z:[-e,-e,-e,-e],y:[e,e,-e,-e]},{t:{r:n,g:n,b:n,u:1},x:[1p,24,24,1p],z:[-e,-e,-e,-e],y:[e,e,-e,-e]},{t:{r:0,g:0,b:0,a:0.2},x:[-1q,1q,1q,-1q],z:[-e,-e,-e,-e],y:[15,15,-I,-I]},{t:{r:0,g:0,b:0,a:0.2},x:[-20,20,20,-20],z:[-e,-e,-e,-e],y:[25,25,15,15]},{t:{r:0,g:0,b:0,a:0.2},x:[-20,20,20,-20],z:[-e,-e,-e,-e],y:[-I,-I,-e,-e]},{t:{r:0,g:0,b:0,a:0.2},x:[-20,20,10,-10],z:[-e,-e,-17,-17],y:[-e,-e,-e,-e]},{t:{r:32,g:32,b:32},x:[-v,v,v,-v],z:[-15,-15,-v,-v],y:[-e,-e,-e,-e]},{t:{r:16,g:16,b:16},x:[-10,10,10,-10],z:[-17,-17,-17,-17],y:[26,26,-e,-e]},{t:{r:n,g:n,b:n},x:[-I,-I,-I,-I],z:[0,-20,-20,0],y:[-V,-V,V,V]},{t:{r:n,g:n,b:n},x:[I,I,I,I],z:[0,-20,-20,0],y:[-V,-V,V,V]},{j:13},{t:{r:n,g:27,b:0},x:[-v,v,v,-v],z:[1r,1r,1s,1s],y:[e,e,e,e]},{t:{r:n,g:27,b:0},x:[-v,v,v,-v],z:[1r,1r,1s,1s],y:[-e,-e,-e,-e]}],1T:{1U:""}});',[],132,'||||||||||||||500|this||||img|||camera|255|var||target|||fill|light|50|||||function|parent|nx|nz|pc|1500|imgs|jpg|320|pointer|scr|if|false|new|ge1doot|Math|1000|ctx|face|for|over|190|transform3D|||||||true|elem|150||100|points|normalZ|normalX|draw|diapo|fps|quality|Poly|Diapo|structure|visible|sqrt|focalLength|json|prototype|else|height|490|420|450|550|layers|Point|poly|drawPoly|||width|move|run|round|load||color|path|isLoaded|loaded|||push|init|dist|lev|isDraging|||imgdata|options|imagesPath|setCursor|projection|texture|return|data|||1001|502|1002|250|300|128'.split('|'),0,{}))
+"use strict";
+
+(function () {
+	/* ==== definitions ==== */
+	var diapo = [], layers = [], ctx, pointer, scr, camera, light, fps = 0, quality = [1,2],
+	// ---- poly constructor ----
+	Poly = function (parent, face) {
+		this.parent = parent;
+		this.ctx    = ctx;
+		this.color  = face.fill || false;
+		this.points = [];
+		if (!face.img) {
+			// ---- create points ----
+			for (var i = 0; i < 4; i++) {
+				this.points[i] = new ge1doot.transform3D.Point(
+					parent.pc.x + (face.x[i] * parent.normalZ) + (face.z[i] * parent.normalX),
+					parent.pc.y +  face.y[i],
+					parent.pc.z + (face.x[i] * parent.normalX) + (-face.z[i] * parent.normalZ)
+				);
+			}
+			this.points[3].next = false;
+		}
+	},
+	// ---- diapo constructor ----
+	Diapo = function (path, img, structure) {
+		// ---- create image ----
+		this.img = new ge1doot.transform3D.Image(
+			this, path + img.img, 1, {
+				isLoaded: function(img) {
+					img.parent.isLoaded = true;
+					img.parent.loaded(img);
+				}
+			}
+		);
+		this.visible  = false;
+		this.normalX  = img.nx;
+		this.normalZ  = img.nz;
+		// ---- point center ----
+		this.pc = new ge1doot.transform3D.Point(img.x, img.y, img.z);
+		// ---- target positions ----
+		this.tx = img.x + (img.nx * Math.sqrt(camera.focalLength) * 20);
+		this.tz = img.z - (img.nz * Math.sqrt(camera.focalLength) * 20);
+		// ---- create polygons ----
+		this.poly = [];
+		for (var i = -1, p; p = structure[++i];) {
+			layers[i] = (p.img === true ? 1 : 2);
+			this.poly.push(
+				new Poly(this, p)
+			);
+		}
+	},
+	// ---- init section ----
+	init = function (json) {
+		// draw poly primitive
+		Poly.prototype.drawPoly = ge1doot.transform3D.drawPoly;
+		// ---- init screen ----
+		scr = new ge1doot.Screen({
+			container: "canvas"
+		});
+		ctx = scr.ctx;
+		scr.resize();
+		// ---- init pointer ----
+		pointer = new ge1doot.Pointer({
+			tap: function () {
+				if (camera.over) {
+					if (camera.over === camera.target.elem) {
+						// ---- return to the center ----
+						camera.target.x = 0;
+						camera.target.z = 0;
+						camera.target.elem = false;
+					} else {
+						// ---- goto diapo ----
+						camera.target.elem = camera.over;
+						camera.target.x = camera.over.tx;
+						camera.target.z = camera.over.tz;
+						// ---- adapt tesselation level to distance ----
+						for (var i = 0, d; d = diapo[i++];) {
+							var dx = camera.target.x - d.pc.x;
+							var dz = camera.target.z - d.pc.z;
+							var dist = Math.sqrt(dx * dx + dz * dz);
+							var lev = (dist > 1500) ? quality[0] : quality[1];
+							d.img.setLevel(lev);
+						}
+					}
+				}
+			}
+		});
+		// ---- init camera ----
+		camera = new ge1doot.transform3D.Camera({
+			focalLength: Math.sqrt(scr.width) * 10,
+			easeTranslation: 0.025,
+			easeRotation: 0.06,
+			disableRz: true
+		}, {
+			move: function () {
+				this.over = false;
+				// ---- rotation ----
+				if (pointer.isDraging) {
+					this.target.elem = false;
+					this.target.ry = -pointer.Xi * 0.01;
+					this.target.rx = (pointer.Y - scr.height * 0.5) / (scr.height * 0.5);
+				} else {
+					if (this.target.elem) {
+						this.target.ry = Math.atan2(
+							this.target.elem.pc.x - this.x,
+							this.target.elem.pc.z - this.z
+						);
+					}
+				}
+				this.target.rx *= 0.9;
+			}
+		});
+		camera.z  = -10000;
+		camera.py = 0;
+		// ---- create images ----
+		for (var i = 0, img; img = json.imgdata[i++];) {
+			diapo.push(
+				new Diapo(
+					json.options.imagesPath, 
+					img, 
+					json.structure
+				)
+			);
+		}	
+		// ---- start engine ---- >>>
+		setInterval(function() {
+			quality = (fps > 50) ? [2,3] : [1,2];
+			fps = 0;
+		}, 1000);
+		run();
+	},
+	// ---- main loop ----
+	run = function () {
+		// ---- clear screen ----
+		ctx.clearRect(0, 0, scr.width, scr.height);
+		// ---- camera ----
+		camera.move();
+		// ---- draw layers ----
+		for (var k = -1, l; l = layers[++k];) {
+			light = false;
+			for (var i = 0, d; d = diapo[i++];) {
+				(l === 1 && d.draw()) || 
+				(d.visible && d.poly[k].draw());
+			}
+		}
+		// ---- cursor ----
+		if (camera.over && !pointer.isDraging) {
+			scr.setCursor("pointer");
+		} else {
+			scr.setCursor("move");
+		}
+		// ---- loop ----
+		fps++;
+		requestAnimFrame(run);
+	};
+	/* ==== prototypes ==== */
+	Poly.prototype.draw = function () {
+		// ---- color light ----
+		var c = this.color;
+		if (c.light || !light) {
+			var s = c.light ? this.parent.light : 1;
+			// ---- rgba color ----
+			light = "rgba(" + 
+				Math.round(c.r * s) + "," +
+				Math.round(c.g * s) + "," + 
+				Math.round(c.b * s) + "," + (c.a || 1) + ")";
+			ctx.fillStyle = light;
+		}
+		// ---- paint poly ----
+		if (!c.light || this.parent.light < 1) {
+			// ---- projection ----
+			for (
+				var i = 0; 
+				this.points[i++].projection();
+			);
+			this.drawPoly();
+			ctx.fill();
+		}
+	}
+	/* ==== image onload ==== */
+	Diapo.prototype.loaded = function (img) {
+		// ---- create points ----
+		var d = [-1,1,1,-1,1,1,-1,-1];
+		var w = img.texture.width  * 0.5;
+		var h = img.texture.height * 0.5;
+		for (var i = 0; i < 4; i++) {
+			img.points[i] = new ge1doot.transform3D.Point(
+				this.pc.x + (w * this.normalZ * d[i]),
+				this.pc.y + (h * d[i + 4]),
+				this.pc.z + (w * this.normalX * d[i])
+			);
+		}
+	}
+	/* ==== images draw ==== */
+	Diapo.prototype.draw = function () {
+		// ---- visibility ----
+		this.pc.projection();
+		if (this.pc.Z > -(camera.focalLength >> 1) && this.img.transform3D(true)) {
+			// ---- light ----
+			this.light = 0.5 + Math.abs(this.normalZ * camera.cosY - this.normalX * camera.sinY) * 0.6;
+			// ---- draw image ----
+			this.visible = true;
+			this.img.draw();
+			// ---- test pointer inside ----
+			if (pointer.hasMoved || pointer.isDown) {
+				if (
+					this.img.isPointerInside(
+						pointer.X,
+						pointer.Y
+					)
+				) camera.over = this;
+			}
+		} else this.visible = false;
+		return true;
+	}
+	return {
+		// --- load data ----
+		load : function (data) {
+			window.addEventListener('load', function () {
+				ge1doot.loadJS(
+					"js/imageTransform3D.js",
+					init, data
+				);
+			}, false);
+		}
+	}
+})
+
+
+
+().load({
+	imgdata:[
+		// north
+		{img:'imgs/1.jpg', x:-1000, y:0, z:1500, nx:0, nz:1},
+		{img:'imgs/2.jpg', x:0,     y:0, z:1500, nx:0, nz:1},
+		{img:'imgs/3.jpg', x:1000,  y:0, z:1500, nx:0, nz:1},
+		// east
+		{img:'imgs/4.jpg', x:1500,  y:0, z:1000, nx:-1, nz:0},
+		{img:'imgs/5.jpg', x:1500,  y:0, z:0, nx:-1, nz:0},
+		{img:'imgs/6.jpg', x:1500,  y:0, z:-1000, nx:-1, nz:0},
+		// south
+		{img:'imgs/7.jpg', x:1000,  y:0, z:-1500, nx:0, nz:-1},
+		{img:'imgs/8.jpg', x:0,     y:0, z:-1500, nx:0, nz:-1},
+		{img:'imgs/9.jpg', x:-1000, y:0, z:-1500, nx:0, nz:-1},
+		// west
+		{img:'imgs/10.jpg', x:-1500, y:0, z:-1000, nx:1, nz:0},
+		{img:'imgs/11.jpg', x:-1500, y:0, z:0, nx:1, nz:0},
+		{img:'imgs/12.jpg', x:-1500, y:0, z:1000, nx:1, nz:0}
+	],
+	structure:[
+		{
+			// wall
+			fill: {r:255, g:255, b:255, light:1},
+			x: [-1001,-490,-490,-1001],
+			z: [-500,-500,-500,-500],
+			y: [500,500,-500,-500]
+		},{
+			// wall
+			fill: {r:255, g:255, b:255, light:1},
+			x: [-501,2,2,-500],
+			z: [-500,-500,-500,-500],
+			y: [500,500,-500,-500]
+		},{
+			// wall
+			fill: {r:255, g:255, b:255, light:1},
+			x: [0,502,502,0],
+			z: [-500,-500,-500,-500],
+			y: [500,500,-500,-500]
+		},{
+			// wall
+			fill: {r:255, g:255, b:255, light:1},
+			x: [490,1002,1002,490],
+			z: [-500,-500,-500,-500],
+			y: [500,500,-500,-500]
+		},{
+			// shadow
+			fill: {r:0, g:0, b:0, a:0.2},
+			x: [-420,420,420,-420],
+			z: [-500,-500,-500,-500],
+			y: [150, 150,-320,-320]
+		},{
+			// shadow
+			fill: {r:0, g:0, b:0, a:0.2},
+			x: [-20,20,20,-20],
+			z: [-500,-500,-500,-500],
+			y: [250, 250,150,150]
+		},{
+			// shadow
+			fill: {r:0, g:0, b:0, a:0.2},
+			x: [-20,20,20,-20],
+			z: [-500,-500,-500,-500],
+			y: [-320, -320,-500,-500]
+		},{
+			// shadow
+			fill: {r:0, g:0, b:0, a:0.2},
+			x: [-20,20,10,-10],
+			z: [-500,-500,-100,-100],
+			y: [-500, -500,-500,-500]
+		},{
+			// base
+			fill: {r:32, g:32, b:32},
+			x: [-50,50,50,-50],
+			z: [-150,-150,-50,-50],
+			y: [-500,-500,-500,-500]
+		},{
+			// support
+			fill: {r:16, g:16, b:16},
+			x: [-10,10,10,-10],
+			z: [-100,-100,-100,-100],
+			y: [300,300,-500,-500]
+		},{
+			// frame
+			fill: {r:255, g:255, b:255},
+			x: [-320,-320,-320,-320],
+			z: [0,-20,-20,0],
+			y: [-190,-190,190,190]
+		},{
+			// frame
+			fill: {r:255, g:255, b:255},
+			x: [320,320,320,320],
+			z: [0,-20,-20,0],
+			y: [-190,-190,190,190]
+		},
+		{img:true},
+		{
+			// ceilingLight
+			fill: {r:255, g:128, b:0},
+			x: [-50,50,50,-50],
+			z: [450,450,550,550],
+			y: [500,500,500,500]
+		},{
+			// groundLight
+			fill: {r:255, g:128, b:0},
+			x: [-50,50,50,-50],
+			z: [450,450,550,550],
+			y: [-500,-500,-500,-500]
+		}
+	],
+	options:{
+		imagesPath: ""
+	}
+});
